8a61543e75cbc41d725787461c56605610363bdb
Makigs <t05612mt@gmail.com>
2023-05-17 00:31:38 +0900

    20230517_0031

--- js/main.js
+++ js/main.js
@@ -22,11 +22,24 @@ $(".branch_id_btn").click(function(){
 
 // fileSelectボタンを押したらfile_reader_inputが発火する
 $(".fileSelect").on("click", function(){
-  if($(".file_reader_input").length){
-    $(".file_reader_input").click();
-    console.log("クリックしたよ")
+  const reader = $(this).siblings(".file_reader_input");
+  if (reader.length) {
+    reader.click();
   }
 });
+
+
+
+
+// クリアボタンが押されたときの処理
+$('.fileClear').on("click", function(){
+  //input,ファイル選択をリセット
+  let table = $(this).siblings('.table');
+  $(this).siblings('.file_reader_input').val('');
+  table.find('.filename').text('ファイルが未選択です'); 
+  table.find('.result_fileName').text('OK/NG'); 
+});
+
 
 // テキストファイルを選択ボタンを押したら
 $('.file_reader_input').change(function (){
@@ -38,24 +51,46 @@ $('.file_reader_input').change(function (){
   //ファイルの情報を代入(file.name=ファイル名/file.size=ファイルサイズ/file.type=ファイルタイプ)
   let fileInfo = $(this).prop('files')[0];
   let fileName = fileInfo.name
-  $('.filename').text(fileName); 
+  let fileName_write = $(this).siblings('.table').find('.filename')
+  fileName_write.text(fileName); 
 
-  // ファイル名チェック
-  if(fileName.includes(branchID) && fileName.includes("SSH・Telnet接続確認")){
-    console.log(branchID,"拠点番号2");
-    $('.result_fileName').text("OK");
+
+  // ファイル名チェックのためのチェックワード関数を定義
+  function fileName_check(context){
+    let checkWord;
+    if($(context).hasClass("no1")){
+      checkWord = "SSH・Telnet接続確認";
+    }else if($(context).hasClass("no2")){
+      checkWord = "エージング";
+    }else if($(context).hasClass("no3")){
+      checkWord = "キッティング後ログ";
+    }else if($(context).hasClass("no4")){
+      checkWord = "ポートチェック";
+    }else if($(context).hasClass("no5")){
+      checkWord = "起動確認";
+    }
+    return checkWord;
+  }
+
+
+  // ファイル名の結果を表示
+  let fileName_result = $(this).siblings('.table').find('.result_fileName')
+  let word = fileName_check(this);
+  console.log(word,"wordに入ってるもの");
+  if(fileName.includes(branchID) && fileName.includes(word)){
+    fileName_result.text("OK");
   } else {
     // 結果にNGを返す
-    console.log(branchID,"拠点番号3");
-    $('.result_fileName').text("NG");
+    fileName_result.text("NG");
   }
+
+
 
 
 
   //読み込み後のkeywordチェック
   reader.onload = function  () {
     let result = reader.result;
-    
     // キーワード以降のテキストを抽出
     let keyword1 = 'SYN arrived interface GigaEthernet1.0';
     // keyword1により文章を2つにわける
@@ -63,106 +98,99 @@ $('.file_reader_input').change(function (){
     // 文章が２つに分かれていた場合
     if (parts1.length == 2) {
       // 結果にOKを返す
-      $('.result_no1').text("OK");
+      $('.result_no1-1').text("OK");
     } else {
       // 結果にNGを返す
-      $('.result_no1').text("NG");
+      $('.result_no1-1').text("NG");
     }
   };
 
 });
 
-// クリアボタンが押されたときの処理
-$('.fileClear').click(function() { 
-  //input,ファイル選択、結果をリセット
-  $('.file_reader_input').val('');
-  $('.filename').text('ファイルが未選択です'); 
-  $('.result_no1').text(''); 
-});
 
 
+// test記載
+// $('#getfile').change(function (){
+//   var file = this.files[0];
+//   //読み込み
+//   var reader = new FileReader();
+//   reader.readAsText(file);
 
-$('#getfile').change(function (){
-  var file = this.files[0];
-  //読み込み
-  var reader = new FileReader();
-  reader.readAsText(file);
-
-  //読み込み後
-  reader.onload = function  () {
-    var result = reader.result;
+//   //読み込み後
+//   reader.onload = function  () {
+//     var result = reader.result;
     
-    // キーワード「xxx」以降のテキストを抽出
-    var keyword1 = '-------------------- show uptime --------------------';
-    var parts1 = result.split(keyword1);
-    if (parts1.length > 1) {
-      // キーワード「yyy」以前のテキストを抽出
-      var keyword2 = '-------------------- show version --------------------';
-      var parts2 = parts1[1].split(keyword2);
-      if (parts2.length > 0) {
-        $('#preview').text(parts2[0]);
-      } else {
-        // キーワードが見つからなかった場合は、ファイル全体を表示
-        $('#preview').text("異なるファイルを読み込んでいます");
-      }
-    } else {
-      // キーワードが見つからなかった場合は、ファイル全体を表示
-      $('#preview').text("異なるファイルを読み込んでいます");
-    }
-  };
-});
+//     // キーワード「xxx」以降のテキストを抽出
+//     var keyword1 = '-------------------- show uptime --------------------';
+//     var parts1 = result.split(keyword1);
+//     if (parts1.length > 1) {
+//       // キーワード「yyy」以前のテキストを抽出
+//       var keyword2 = '-------------------- show version --------------------';
+//       var parts2 = parts1[1].split(keyword2);
+//       if (parts2.length > 0) {
+//         $('#preview').text(parts2[0]);
+//       } else {
+//         // キーワードが見つからなかった場合は、ファイル全体を表示
+//         $('#preview').text("異なるファイルを読み込んでいます");
+//       }
+//     } else {
+//       // キーワードが見つからなかった場合は、ファイル全体を表示
+//       $('#preview').text("異なるファイルを読み込んでいます");
+//     }
+//   };
+// });
 
 
 // 試しコード
-function handleFileChange(e) {
-  let file = e.target.files[0];
-  console.log(file);
+// function handleFileChange(e) {
+//   let file = e.target.files[0];
+//   console.log(file);
 
-  // ファイルの読み込み
-  readFile(file);
+//   // ファイルの読み込み
+//   readFile(file);
 
-  // ファイル情報を取得
-  let fileInfo = getFileInfo(file);
+//   // ファイル情報を取得
+//   let fileInfo = getFileInfo(file);
   
-  // ファイル名チェック
-  checkFileName(fileInfo.name);
-}
+//   // ファイル名チェック
+//   checkFileName(fileInfo.name);
+// }
 
-function readFile(file) {
-  let reader = new FileReader();
-  reader.onload = function  () {
-    checkKeyword(reader.result);
-  };
-  reader.readAsText(file);
-}
+// function readFile(file) {
+//   let reader = new FileReader();
+//   reader.onload = function  () {
+//     checkKeyword(reader.result);
+//   };
+//   reader.readAsText(file);
+// }
 
-function getFileInfo(file) {
-  let fileInfo = file;
-  let fileName = fileInfo.name;
-  $('.filename').text(fileName);
-  return fileInfo;
-}
+// function getFileInfo(file) {
+//   let fileInfo = file;
+//   let fileName = fileInfo.name;
+//   $('.filename').text(fileName);
+//   return fileInfo;
+// }
 
-function checkFileName(fileName) {
-  if(fileName.includes(branchID) && fileName.includes("SSH・Telnet接続確認")){
-    console.log(branchID, "拠点番号2");
-    $('.result_fileName').text("OK");
-  } else {
-    console.log(branchID, "拠点番号3");
-    $('.result_fileName').text("NG");
-  }
-}
+// function checkFileName(fileName) {
+//   if(fileName.includes(branchID) && fileName.includes("SSH・Telnet接続確認")){
+//     console.log(branchID, "拠点番号2");
+//     $('.result_fileName').text("OK");
+//   } else {
+//     console.log(branchID, "拠点番号3");
+//     $('.result_fileName').text("NG");
+//   }
+// }
 
-function checkKeyword(result) {
-  let keyword1 = 'SYN arrived interface GigaEthernet1.0';
-  let parts1 = result.split(keyword1);
-  if (parts1.length == 2) {
-    $('.result_no1').text("OK");
-  } else {
-    $('.result_no1').text("NG");
-  }
-}
+// function checkKeyword(result) {
+//   let keyword1 = 'SYN arrived interface GigaEthernet1.0';
+//   let parts1 = result.split(keyword1);
+//   if (parts1.length == 2) {
+//     $('.result_no1').text("OK");
+//   } else {
+//     $('.result_no1').text("NG");
+//   }
+// }
 
-$('.file_reader_input').change(handleFileChange);
+// $('.file_reader_input').change(handleFileChange);
 
 
