a0c7b7163ef4741991669dbd0bcad1e2050acec1
Makigs <t05612mt@gmail.com>
2023-05-18 00:44:12 +0900

    20230518_0044

--- js/main.js
+++ js/main.js
@@ -5,14 +5,14 @@ $(".branch_id_btn").click(function(){
   var flag = 0;
 
   // 半角数字で入力されているかチェック
-  if ($(".branch_Num_Input").val().match(/[^0-9]+/)){ // 半角数字で入力されていない場合
+  if ($(".branch_Num_Input").val().match(/[^0-9]+/)){ 
       flag = 1;
   }
   console.log(flag,"flagの数字");
 
   if(flag == 1 ){ // 半角数字で入力されていない場合
-      window.alert('半角数字のみ入力できます'); // アラートを表示
-      return false; // 送信中止
+      alert('半角数字のみ入力できます');
+      return false;
   }else{ // 半角数字で入力されている場合
     branchID = $(".branch_Num_Input").val();
     console.log(branchID,"拠点番号");
@@ -28,7 +28,15 @@ $(".fileSelect").on("click", function(){
   }
 });
 
-
+// すべてクリアボタンを押したときの処理
+$('.allClear').on("click", function(){
+  $('.branch_id_view').text('');
+  $('.branch_Num_Input').val('');
+  $('.filename').text('ファイル名');
+  $('.result_fileName').text('OK/NG');
+  $('.cell_2').text('');
+  $('.cell_3').text('');
+});
 
 
 // クリアボタンが押されたときの処理
@@ -38,6 +46,34 @@ $('.fileClear').on("click", function(){
   $(this).siblings('.file_reader_input').val('');
   table.find('.filename').text('ファイルが未選択です'); 
   table.find('.result_fileName').text('OK/NG'); 
+
+  function result_delete(context){
+    let sibling = $(context).siblings('.file_reader_input');
+    if(sibling.hasClass("no1")){
+      $('.result_no1-1').text(''); 
+      $('.result_no1-2').text(''); 
+      $('.result_no1-3').text(''); 
+
+    }else if(sibling.hasClass("no2")){
+      $('.result_no2-1').text(''); 
+
+    }else if(sibling.hasClass("no3")){
+      $('.result_no3-1').text(''); 
+
+    }else if(sibling.hasClass("no4")){
+      $('.result_no4-1').text(''); 
+      $('.result_no4-2').text(''); 
+      $('.result_no4-3').text(''); 
+      $('.result_no4-4').text(''); 
+      $('.result_no4-5').text(''); 
+
+    }else if(sibling.hasClass("no5")){
+      $('.result_no5-1').text(''); 
+      $('.result_no5-2').text(''); 
+    }
+  }
+
+  result_delete(this);
 });
 
 
@@ -48,7 +84,7 @@ $('.file_reader_input').change(function (){
   //ファイル読み込み
   let reader = new FileReader();
   reader.readAsText(file);
-  //ファイルの情報を代入(file.name=ファイル名/file.size=ファイルサイズ/file.type=ファイルタイプ)
+  //ファイルの情報を代入(file.name=ファイル名)
   let fileInfo = $(this).prop('files')[0];
   let fileName = fileInfo.name
   let fileName_write = $(this).siblings('.table').find('.filename')
@@ -82,6 +118,7 @@ $('.file_reader_input').change(function (){
   } else {
     // 結果にNGを返す
     fileName_result.text("NG");
+    return;
   }
 
 
@@ -123,16 +160,43 @@ $('.file_reader_input').change(function (){
       if(finalResult.includes('22') && finalResult.includes('23')) {
         $('.result_no1-2').text("OK");
         $('.result_no1-3').text("OK");
-      } else {
+      } else if(!finalResult.includes('22') && finalResult.includes('23')){
+        $('.result_no1-2').text("NG");
+        $('.result_no1-3').text("OK");
+      } else if(finalResult.includes('22') && !finalResult.includes('23')){
+        $('.result_no1-2').text("OK");
+        $('.result_no1-3').text("NG");
+      } else{
         $('.result_no1-2').text("NG");
         $('.result_no1-3').text("NG");
       }
+
+
       
     
   // 2.エージング確認
     }else if($(this_input).hasClass("no2")){
       let result2 = reader.result;
       console.log(result2);
+
+      let keyword2_1 = '------ show uptime ------';
+      let keyword2_2 = '------ show version ------';
+      let keyword2_3 = 'System uptime is ';
+      let keyword2_4 = 'System woke up by reload';
+
+      let parts2_1 = result2.split(keyword2_1);
+      let parts2_2 = parts2_1[1].split(keyword2_2);
+      let show_uptime = parts2_2[0];
+
+      console.log(show_uptime);
+
+      let start_ut = (show_uptime.indexOf(keyword2_3)) + 17;
+      let end_ut = show_uptime.indexOf(keyword2_4);
+      let result_ut = show_uptime.substring(start_ut, end_ut);
+      console.log(result_ut);
+      $('.result_no2-1').text(result_ut);
+
+
 
   // 3.キッティング後ログ確認
     }else if($(this_input).hasClass("no3")){
@@ -159,11 +223,65 @@ $('.file_reader_input').change(function (){
         $('.result_no3-1').text("NG");
       }
 
-
+  // 4.ポートチェック確認
     }else if($(this_input).hasClass("no4")){
       let result4 = reader.result;
       console.log(result4);
 
+      let keyword4_1 = 'Link status up for port 0, 1G b/s, GigaEthernet0';
+      let keyword4_2 = 'Link status down for port 0, GigaEthernet0';
+      let keyword4_3 = 'Link status up for port 1, 1G b/s, GigaEthernet1';
+      let keyword4_4 = 'Link status down for port 1, GigaEthernet1';
+      let keyword4_5 = 'Link status up for port 2, 1G b/s, GigaEthernet1';
+      let keyword4_6 = 'Link status down for port 2, GigaEthernet1';
+      let keyword4_7 = 'Link status up for port 3, 1G b/s, GigaEthernet1';
+      let keyword4_8 = 'Link status down for port 3, GigaEthernet1';
+      let keyword4_9 = 'Link status up for port 4, 1G b/s, GigaEthernet1';
+      let keyword4_10 = 'Link status down for port 4, GigaEthernet1';
+
+      let r4_1 = result4.indexOf(keyword4_1);
+      let r4_2 = result4.indexOf(keyword4_2);
+      let r4_3 = result4.indexOf(keyword4_3);
+      let r4_4 = result4.indexOf(keyword4_4);
+      let r4_5 = result4.indexOf(keyword4_5);
+      let r4_6 = result4.indexOf(keyword4_6);
+      let r4_7 = result4.indexOf(keyword4_7);
+      let r4_8 = result4.indexOf(keyword4_8);
+      let r4_9 = result4.indexOf(keyword4_9);
+      let r4_10 = result4.indexOf(keyword4_10);
+
+      
+      if(r4_1==-1 || r4_2==-1){
+        $('.result_no4-1').text("NG");
+      }else{
+        $('.result_no4-1').text("OK");
+      }
+
+      if(r4_3==-1 || r4_4==-1){
+        $('.result_no4-2').text("NG");
+      }else{
+        $('.result_no4-2').text("OK");
+      }
+
+      if(r4_5==-1 || r4_6==-1){
+        $('.result_no4-3').text("NG");
+      }else{
+        $('.result_no4-3').text("OK");
+      }
+
+      if(r4_7==-1 || r4_8==-1){
+        $('.result_no4-4').text("NG");
+      }else{
+        $('.result_no4-4').text("OK");
+      }
+
+      if(r4_9==-1 || r4_10==-1){
+        $('.result_no4-5').text("NG");
+      }else{
+        $('.result_no4-5').text("OK");
+      }
+  
+  // 5.起動確認
     }else if($(this_input).hasClass("no5")){
       let result5 = reader.result;
       console.log(result5);
@@ -174,26 +292,20 @@ $('.file_reader_input').change(function (){
 
       // keyword1により文章を2つにわける
       let parts5_1 = result5.split(keyword5_1);
-      let parts5_2 = parts5_1[1].split(keyword5_2);
-      let parts5_3 = parts5_2[1].split(keyword5_3);
 
-      let start_ver = part5_2.indexOf(keyword5_2);
-      let end_ver = ;
+      let start_ver = parts5_1[1].indexOf(keyword5_2);
+      let end_ver = start_ver + 15;
+      let result_ver = parts5_1[1].substring(start_ver, end_ver);
+      console.log(result_ver);
+      $('.result_no5-1').text(result_ver);
+      
 
-      let result_ver = result1.substring(start, end);
-
-      // Verの取得
-      if (parts5_2.length == 2) {
-        // 結果にOKを返す
-        $('.result_no3-1').text("OK");
-      } else {
-        // 結果にNGを返す
-        $('.result_no3-1').text("NG");
-      }
-
-
-
-
+      let start_sn = (parts5_1[1].indexOf(keyword5_3)) + 5;
+      let end_sn = start_sn + 12;
+      console.log(end_sn)
+      let result_sn = parts5_1[1].substring(start_sn, end_sn);
+      console.log(result_sn);
+      $('.result_no5-2').text(result_sn);
 
     }
   }
